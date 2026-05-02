653d85d80f7236446cc1e406bf94ff12188a7821
sditqurrataayun2014-cpu <sditqurrataayun2014@gmail.com>
2026-05-02 09:30:33 +0700

    Upload pertama aplikasi PayEdu

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'pwa-192x192.png', 'pwa-512x512.png'],
+      manifest: {
+        name: 'PayEdu - Portal Penggajian',
+        short_name: 'PayEdu',
+        description: 'Sistem Informasi SDM dan Finansial Sekolah',
+        theme_color: '#2563eb', // Warna header di peramban HP
+        background_color: '#f8fafc',
+        display: 'standalone', // Hilangkan address bar saat diinstal
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
+})
